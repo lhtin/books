a4858ad469483b231167c59ff6ffff670466c10e
Lehua Ding <lehuading@tencent.com>
2026-01-27 00:01:52 +0800

    build html

--- html/searcher-c2a407aa.js
+++ html/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-cfd7d6de.js',
+                path_to_root + 'searchindex-57008fe7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
