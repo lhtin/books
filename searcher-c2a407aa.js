db547df1554703a7c33fb9455e95bcabba7e6983
Lehua Ding <lehuading@qq.com>
2026-05-11 13:27:42 +0800

    build html

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-3db4c842.js',
+                path_to_root + 'searchindex-00e9a542.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
